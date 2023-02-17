fe4760031a772526fc2a15d453697d1c0ffc1abb
Eric Villarreal <35volts+myreprise1@protonmail.com>
2023-02-16 20:03:05 -0600

    Make `NamedField` and `UnnamedField` templated.

--- crates/bevy_reflect/src/fields.rs
+++ crates/bevy_reflect/src/fields.rs
@@ -2,94 +2,36 @@ use crate::Reflect;
 use std::any::{Any, TypeId};
 
 /// The named field of a reflected struct.
-#[derive(Clone, Debug)]
-pub struct NamedField {
-    name: &'static str,
-    type_name: &'static str,
-    type_id: TypeId,
-    #[cfg(feature = "documentation")]
-    docs: Option<&'static str>,
-}
-
-impl NamedField {
-    /// Create a new [`NamedField`].
-    pub fn new<T: Reflect>(name: &'static str) -> Self {
-        Self {
-            name,
-            type_name: std::any::type_name::<T>(),
-            type_id: TypeId::of::<T>(),
-            #[cfg(feature = "documentation")]
-            docs: None,
-        }
-    }
-
-    /// Sets the docstring for this field.
-    #[cfg(feature = "documentation")]
-    pub fn with_docs(self, docs: Option<&'static str>) -> Self {
-        Self { docs, ..self }
-    }
-
-    /// The name of the field.
-    pub fn name(&self) -> &'static str {
-        self.name
-    }
-
-    /// The [type name] of the field.
-    ///
-    /// [type name]: std::any::type_name
-    pub fn type_name(&self) -> &'static str {
-        self.type_name
-    }
-
-    /// The [`TypeId`] of the field.
-    pub fn type_id(&self) -> TypeId {
-        self.type_id
-    }
-
-    /// Check if the given type matches the field type.
-    pub fn is<T: Any>(&self) -> bool {
-        TypeId::of::<T>() == self.type_id
-    }
-
-    /// The docstring of this field, if any.
-    #[cfg(feature = "documentation")]
-    pub fn docs(&self) -> Option<&'static str> {
-        self.docs
-    }
-}
+pub type NamedField = Field<&'static str>;
 
 /// The unnamed field of a reflected tuple or tuple struct.
+pub type UnnamedField = Field<usize>;
+
+/// A field of a reflected struct, tuple, or tuple struct.
+/// 
+/// Named fields have a key of `&'static str`, accessed by `name`.
+/// Unnamed fields (like in tuples and tuple structs) have a key of `usize`, accessed by `index`.
 #[derive(Clone, Debug)]
-pub struct UnnamedField {
-    index: usize,
+pub struct Field<Key> {
+    key: Key,
     type_name: &'static str,
     type_id: TypeId,
     #[cfg(feature = "documentation")]
     docs: Option<&'static str>,
 }
 
-impl UnnamedField {
-    pub fn new<T: Reflect>(index: usize) -> Self {
+impl<Key> Field<Key> {
+    /// Create a new [`StructField`].
+    pub fn new<T: Reflect>(key: Key) -> Self {
         Self {
-            index,
+            key,
             type_name: std::any::type_name::<T>(),
             type_id: TypeId::of::<T>(),
             #[cfg(feature = "documentation")]
             docs: None,
         }
     }
-
-    /// Sets the docstring for this field.
-    #[cfg(feature = "documentation")]
-    pub fn with_docs(self, docs: Option<&'static str>) -> Self {
-        Self { docs, ..self }
-    }
-
-    /// Returns the index of the field.
-    pub fn index(&self) -> usize {
-        self.index
-    }
-
+    
     /// The [type name] of the field.
     ///
     /// [type name]: std::any::type_name
@@ -107,9 +49,29 @@ impl UnnamedField {
         TypeId::of::<T>() == self.type_id
     }
 
+    /// Sets the docstring for this field.
+    #[cfg(feature = "documentation")]
+    pub fn with_docs(self, docs: Option<&'static str>) -> Self {
+        Self { docs, ..self }
+    }
+
     /// The docstring of this field, if any.
     #[cfg(feature = "documentation")]
     pub fn docs(&self) -> Option<&'static str> {
         self.docs
+    }
+}
+
+impl Field<&'static str> {
+    /// Returns the name of the field.
+    pub fn name(&self) -> &'static str {
+        self.key
+    }
+}
+
+impl Field<usize> {
+    /// Returns the index of the field.
+    pub fn index(&self) -> usize {
+        self.key
     }
 }
